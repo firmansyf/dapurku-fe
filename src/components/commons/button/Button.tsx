76ed4d6eacd4d9c90fc7af0f5d032c18949045c3
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2024-12-28 20:35:06 +0700

    feat: create-admin-page-and-functional

--- src/components/commons/button/Button.tsx
+++ src/components/commons/button/Button.tsx
@@ -20,7 +20,7 @@ const Button: React.FC<ButtonProps> = ({
   className = ''
 }) => {
   const baseStyles =
-    'font-medium rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 transition duration-300';
+    'font-medium rounded-lg focus:outline-none focus:ring-1 focus:ring-offset-2 transition duration-300';
 
   const sizeStyles = {
     sm: 'px-3 py-2 text-sm',
