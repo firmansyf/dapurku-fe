cfd7176ecf37a78fc86e9d98fdefc537ff83fe75
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2024-12-30 17:07:01 +0700

    feat: create-module-produc-management-and-fix-datatable

--- src/containers/Admin/product-management/header/index.tsx
+++ src/containers/Admin/product-management/header/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 
 const Header: FC = () => {
     return (
-        <div className='bg-[#EEE] p-7'>
+        <div className='bg-[#EEE] p-7 sticky top-14'>
             <h1 className='text-xl font-bold'>Products Management</h1>
         </div>
     )
